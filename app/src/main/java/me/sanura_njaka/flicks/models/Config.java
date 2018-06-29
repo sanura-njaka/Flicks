2b9e9177f747cf328fc9101358ce7be2cf47456c
sanura-njaka <sanura@stanford.edu>
2018-06-29 15:54:46 -0700

    background black

--- app/src/main/java/me/sanura_njaka/flicks/models/Config.java
+++ app/src/main/java/me/sanura_njaka/flicks/models/Config.java
@@ -6,9 +6,11 @@ import org.json.JSONObject;
 
 public class Config {
     // the base url for loading images
-    String imageBaseUrl;
+    private String imageBaseUrl;
     // the poster size to use when fetching images, part of the url
-    String posterSize;
+    private String posterSize;
+    // the backdrop size to use when fetching images
+    private String backdropSize;
 
     public Config(JSONObject object) throws JSONException {
         JSONObject images = object.getJSONObject("images");
@@ -18,6 +20,9 @@ public class Config {
         JSONArray posterSizeOptions = images.getJSONArray("poster_sizes");
         // use the option at index 3 or w342 as a fallback
         posterSize = posterSizeOptions.optString(3, "w342");
+        // parse the backdrop sizes and use the option at index1 or w780 as a fallback
+        JSONArray backdropSizeOptions = images.getJSONArray("backdrop_sizes");
+        backdropSize = backdropSizeOptions.optString(1, "w780");
     }
 
     // helper method for creating urls
